d138020c5419800cfd91c8f51d3b73f78c751b4a
Matthew <matthewhudghton@gmail.com>
2021-05-17 20:43:04 +0100

    disabling lights on projectiles

--- src/projectile.js
+++ src/projectile.js
@@ -45,16 +45,19 @@ export class Projectile extends Actor {
       })
     );
 
-    const light = new this.THREE.PointLight(
-      this.color,
-      this.size * this.size,
-      0,
-      2
-    );
-    light.position.set(0, 0, 0);
-    this.lights.push(light);
-    this.mesh.add(light);
+    if (false && Math.random() * 15 < this.size) {
+      const light = new this.THREE.PointLight(
+        this.color,
+        this.size * this.size,
+        0,
+        2
+      );
 
+      light.position.set(0, 0, 0);
+      this.lights.push(light);
+
+      this.mesh.add(light);
+    }
     this.sounds.push(
       new Sound({
         THREE: this.THREE,
